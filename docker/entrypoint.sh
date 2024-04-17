70c4b9059abcf9dcd511207e8f69cbb552f67ef3
murphy83 <github@murphyslantech.de>
2024-04-17 18:44:55 +0200

    Update entrypoint.sh
    Added Options to run TLS natively

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,9 +8,15 @@ GUNICORN_LOGLEVEL="${GUNICORN_LOGLEVEL:-info}"
 BIND_ADDRESS="${BIND_ADDRESS:-0.0.0.0:80}"
 
 GUNICORN_ARGS="-t ${GUNICORN_TIMEOUT} --workers ${GUNICORN_WORKERS} --bind ${BIND_ADDRESS} --log-level ${GUNICORN_LOGLEVEL}"
+
+GUNITORN_TLS_ARGS=""
+if [ $TLS_ENABLE != "false"]; then
+  GUNITORN_TLS_ARGS="-keyfile ${TLS_KEYFILE} --certfile ${TLS_CERT_FILE} --ca-certs ${TLS_CHAIN_FILE}"
+fi
+
 if [ "$1" == gunicorn ]; then
     /bin/sh -c "flask db upgrade"
-    exec "$@" $GUNICORN_ARGS
+    exec "$@" $GUNICORN_ARGS $GUNITORN_TLS_ARGS
 
 else
     exec "$@"
